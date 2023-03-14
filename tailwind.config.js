b052a326b18988bae119906ea0b6d7f7621501c4
alizare <a.zare@Tse.ir>
2023-03-14 12:08:58 +0330

    install tailwind and set this config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      white: '#fff',
+      black: '#000',
+      navy: '#394562',
+      pink: '#ea4c89',
+      lightBlue: '#f2f4f9',
+    },
+  },
+  plugins: [],
+};
