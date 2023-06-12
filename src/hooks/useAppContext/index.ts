1f98c3c5bad17eb8d75fe5af0a27231a618148ef
alizare <a.zare@Tse.ir>
2023-06-12 11:14:19 +0330

    fix bug

--- src/hooks/useAppContext/index.ts
+++ src/hooks/useAppContext/index.ts
@@ -2,6 +2,6 @@ import { useContext } from 'react';
 import { store } from '../../storage';
 import {StoreType} from '@enums';
 
-export const useAppContext = (): StoreType => {
+export const useAppContext = (): any => {
   return useContext(store);
 };
