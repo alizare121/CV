6fff86553fda7da8abc722dcff769f36ab33a7c6
alizare121 <aliiizaareee@gmail.com>
2023-12-17 16:24:24 +0330

    Fix: add meta tag in head

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,13 +1,25 @@
 import Head from 'next/head';
 
 export function Header() {
+  const title = 'Ali Zare - Software Developer Resume';
+  const description =
+    'Explore the resume and professional portfolio of Ali Zare, showcasing skills, experiences, and projects.';
   return (
     <Head>
-      <title>Ali Zare CV</title>
-      <meta name='description' content='Ali Zare CV' />
+      <title>{title}</title>
+      <meta name='description' content={description} />
       <meta name='viewport' content='width=1224' />
       <link rel='icon' href='/dev-icon.png' />
-      <link rel='preload' href='/fonts/IRANSans.ttf' as='font' />
+      <link
+        rel='preload'
+        href='/fonts/IRANSans.ttf'
+        as='font'
+        crossOrigin='anonymous'
+      />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content='website' />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 }
