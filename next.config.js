33e036e80fb656caad60bb00b668988c6a1e1d7b
Ali Zare <A.zare@charisma.tech>
2024-06-19 10:55:16 +0330

    Fix: Fix split chunks in next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
