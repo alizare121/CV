4a6bb9582f4a83f301a941cac3eeeab787d25a02
alizare121 <aliiizaareee@gmail.com>
2025-01-14 17:43:18 +0330

    Fix: re design skills section

--- src/components/PageFeature/Skills.tsx
+++ src/components/PageFeature/Skills.tsx
@@ -1,92 +1,194 @@
-import Image from "next/image";
-import personal2 from "../../assets/images/dev1.png";
-import { Slider } from "@components";
-import { useAppContext } from "@utils";
-import { SkillsInterface } from "@enums";
+import Image from 'next/image';
+import personal2 from '../../assets/images/dev1.png';
+import { Slider } from '@components';
+import { useAppContext } from '@utils';
+import { SkillsInterface } from '@enums';
 export function Skills({ isEn }: { isEn: boolean }) {
   const { messages } = useAppContext();
-  const scale = isEn ? "" : "scale-x-[-1]";
 
-  const data: SkillsInterface[] = [
+  const badges = [
     {
-      id: 1,
-      title: "Development",
-      icon: "code",
-      data: [
-        "JavaScript (ES6 & Beyond) ",
-        "TypeScript",
-        "React JS",
-        "React Native",
-        "Next js",
-        "State Management : Redux(thunk), Context Api, MobX, Recoil , jotai",
-        "HTML, CSS, SASS, Style-jsx, emotion, Post-css",
-        "Bootstrap, Material-Ui, Ant Design, Styled-Components, tailwind",
-        "Test : Unit Test (jest), Integration Test(jest), End to End Test(puppeteer)",
-        "Webpack , Vite.js",
-      ],
+      src: 'https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E',
+      alt: 'JavaScript (ES6 & Beyond)',
     },
     {
-      id: 2,
-      title: "Tools",
-      icon: "wrench",
-      data: [
-        "Git , Git Flow , monorepo , husky , Submodules",
-        "Firebase (Push Notification , Analytics , Crashlytics) , Tag Manager",
-        "Update (CodePush)",
-        "Monitoring ( Sentry )",
-        "Documentation ( StoryBook )",
-        "Scrum methodology , Kanban",
-      ],
+      src: 'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white',
+      alt: 'TypeScript',
     },
     {
-      id: 3,
-      title: "Blockchain",
-      icon: "blockchain",
-      data: ["Solidity", "Blockchain", "Ethereum", "Smart Contract"],
-      note: "I am learning and junior in this field",
+      src: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+      alt: 'React JS',
     },
     {
-      id: 4,
-      title: "Network",
-      icon: "monitor",
-      data: ["MikroTik", "MCITP", "Linux", "Docker"],
+      src: 'https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+      alt: 'React Native',
     },
     {
-      id: 5,
-      title: "Soft Skills",
-      icon: "user",
-      data: [
-        messages.softSkill1,
-        messages.softSkill2,
-        messages.softSkill3,
-        messages.softSkill4,
-        messages.softSkill5,
-        messages.softSkill6,
-        messages.softSkill7,
-        messages.softSkill8,
-        messages.softSkill9,
-      ],
+      src: 'https://img.shields.io/badge/next-black?style=for-the-badge&logo=next.js&logoColor=white',
+      alt: 'Next.js',
+    },
+    {
+      src: 'https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white',
+      alt: 'Redux (Thunk)',
+    },
+    {
+      src: 'https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white',
+      alt: 'React Router',
+    },
+    {
+      src: 'https://img.shields.io/badge/React%20Hook%20Form-%23EC5990.svg?style=for-the-badge&logo=reacthookform&logoColor=white',
+      alt: 'React Hook Form',
+    },
+    {
+      src: 'https://img.shields.io/badge/Context%20API-%23000000.svg?style=for-the-badge&logo=react&logoColor=white',
+      alt: 'Context API',
+    },
+    {
+      src: 'https://img.shields.io/badge/mobx-%23FF9955.svg?style=for-the-badge&logo=mobx&logoColor=black',
+      alt: 'MobX',
+    },
+    {
+      src: 'https://img.shields.io/badge/recoil-%23FF4242.svg?style=for-the-badge&logo=recoil&logoColor=white',
+      alt: 'Recoil',
+    },
+    {
+      src: 'https://img.shields.io/badge/jotai-%23F7C12E.svg?style=for-the-badge&logo=jotai&logoColor=black',
+      alt: 'Jotai',
+    },
+    {
+      src: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+      alt: 'HTML',
+    },
+    {
+      src: 'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
+      alt: 'CSS',
+    },
+    {
+      src: 'https://img.shields.io/badge/sass-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white',
+      alt: 'SASS',
+    },
+    {
+      src: 'https://img.shields.io/badge/style--jsx-%23FF4081.svg?style=for-the-badge&logo=react&logoColor=white',
+      alt: 'Style-jsx',
+    },
+    {
+      src: 'https://img.shields.io/badge/emotion-%2361DAFB.svg?style=for-the-badge&logo=emotion&logoColor=white',
+      alt: 'Emotion',
+    },
+    {
+      src: 'https://img.shields.io/badge/postcss-%23DD3A0A.svg?style=for-the-badge&logo=postcss&logoColor=white',
+      alt: 'Post-CSS',
+    },
+    {
+      src: 'https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white',
+      alt: 'Bootstrap',
+    },
+    {
+      src: 'https://img.shields.io/badge/material--ui-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white',
+      alt: 'Material-UI',
+    },
+    {
+      src: 'https://img.shields.io/badge/ant%20design-%2300A0B0.svg?style=for-the-badge&logo=ant-design&logoColor=white',
+      alt: 'Ant Design',
+    },
+    {
+      src: 'https://img.shields.io/badge/styled--components-%23DB7093.svg?style=for-the-badge&logo=styled-components&logoColor=white',
+      alt: 'Styled-Components',
+    },
+    {
+      src: 'https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white',
+      alt: 'TailwindCSS',
+    },
+    {
+      src: 'https://img.shields.io/badge/jest-%23C21325.svg?style=for-the-badge&logo=jest&logoColor=white',
+      alt: 'Jest (Unit Test)',
+    },
+    {
+      src: 'https://img.shields.io/badge/puppeteer-%23010000.svg?style=for-the-badge&logo=puppeteer&logoColor=white',
+      alt: 'Puppeteer (End to End Test)',
+    },
+    {
+      src: 'https://img.shields.io/badge/webpack-%238DD6F9.svg?style=for-the-badge&logo=webpack&logoColor=black',
+      alt: 'Webpack',
+    },
+    {
+      src: 'https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white',
+      alt: 'Vite.js',
+    },
+    {
+      src: 'https://img.shields.io/badge/git-%23F05032.svg?style=for-the-badge&logo=git&logoColor=white',
+      alt: 'Git',
+    },
+    {
+      src: 'https://img.shields.io/badge/git--flow-%23F05032.svg?style=for-the-badge&logo=git&logoColor=white',
+      alt: 'Git Flow',
+    },
+    {
+      src: 'https://img.shields.io/badge/monorepo-%23FF6200.svg?style=for-the-badge&logo=google&logoColor=white',
+      alt: 'Monorepo',
+    },
+    {
+      src: 'https://img.shields.io/badge/husky-%23FFB000.svg?style=for-the-badge&logo=husky&logoColor=black',
+      alt: 'Husky',
+    },
+    {
+      src: 'https://img.shields.io/badge/firebase-%23FFCA28.svg?style=for-the-badge&logo=firebase&logoColor=black',
+      alt: 'Firebase',
+    },
+    {
+      src: 'https://img.shields.io/badge/codepush-%2300C4B3.svg?style=for-the-badge&logo=react&logoColor=white',
+      alt: 'CodePush (Update)',
+    },
+    {
+      src: 'https://img.shields.io/badge/sentry-%23E15B4A.svg?style=for-the-badge&logo=sentry&logoColor=white',
+      alt: 'Sentry (Monitoring)',
+    },
+    {
+      src: 'https://img.shields.io/badge/storybook-%23FF4785.svg?style=for-the-badge&logo=storybook&logoColor=white',
+      alt: 'Storybook (Documentation)',
+    },
+    {
+      src: 'https://img.shields.io/badge/scrum-%23FF6F00.svg?style=for-the-badge&logo=scrum&logoColor=white',
+      alt: 'Scrum Methodology',
+    },
+    {
+      src: 'https://img.shields.io/badge/kanban-%23E80000.svg?style=for-the-badge&logo=kanban&logoColor=white',
+      alt: 'Kanban',
+    },
+    {
+      src: 'https://img.shields.io/badge/mikrotik-%2393CC7B.svg?style=for-the-badge&logo=mikrotik&logoColor=white',
+      alt: 'MikroTik',
+    },
+    {
+      src: 'https://img.shields.io/badge/mcitp-%23F35F5F.svg?style=for-the-badge&logo=microsoft&logoColor=white',
+      alt: 'MCITP',
+    },
+    {
+      src: 'https://img.shields.io/badge/linux-%23FCC624.svg?style=for-the-badge&logo=linux&logoColor=black',
+      alt: 'Linux',
+    },
+    {
+      src: 'https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white',
+      alt: 'Docker',
     },
   ];
   return (
     <section>
-      <div className="mt-20 flex flex-col items-center justify-center mb-[-3rem]">
-        <span className="text-pink">{messages.expertise}</span>
-        <span className="text-4xl text-navy font-bold">{messages.skills}</span>
+      <div className='mt-20 flex flex-col items-center justify-center'>
+        <span className='text-white'>{messages.expertise}</span>
+        <span className='text-4xl text-yellow font-bold'>
+          {messages.skills}
+        </span>
       </div>
-      <div className="flex flex-row items-center mx-20">
-        <div className="w-[40%] flex items-center justify-center">
-          <Image
-            src={personal2}
-            alt="ali"
-            width={500}
-            height={300}
-            className={`me-20 ${scale} `}
+      <div className='mt-8 flex flex-wrap gap-4'>
+        {badges.map((badge, index) => (
+          <img
+            key={index}
+            src={badge.src}
+            alt={badge.alt}
+            className='h-[40px]'
           />
-        </div>
-        <div className="flex-1">
-          <Slider data={data} />
-        </div>
+        ))}
       </div>
     </section>
   );
