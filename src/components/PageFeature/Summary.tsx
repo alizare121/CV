965de26db1c11ce463e75ee2dd5d5a5d28b4d413
alizare121 <aliiizaareee@gmail.com>
2025-01-14 18:42:25 +0330

    Fix: Fix bug in summary bg

--- src/components/PageFeature/Summary.tsx
+++ src/components/PageFeature/Summary.tsx
@@ -6,7 +6,7 @@ export function Summary() {
   return (
     <div className='mt-20 flex justify-center flex-col items-center text-center gap-4'>
       <span className='text-4xl font-bold text-yellow'>About Me</span>
-      <div className='bg-white flex bg-darkGray flex-col w-full rounded-2xl p-8 shadow-md'>
+      <div className='flex bg-darkGray flex-col w-full rounded-2xl p-8'>
         <p className='text-justify text-white text-xl leading-8'>
           {messages.summary}
         </p>
