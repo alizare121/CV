6ea80782b927d3dbb0f016388a8c51d69ef71aa7
alizare121 <aliiizaareee@gmail.com>
2025-01-14 18:14:10 +0330

    Fix: Fix re design edu and about me section

--- src/components/PageFeature/AboutMe.tsx
+++ src/components/PageFeature/AboutMe.tsx
@@ -5,7 +5,7 @@ import ali2 from '../../assets/images/ali3.png';
 export function AboutMe() {
   const { messages } = useAppContext();
   return (
-    <div className='flex flex-row mt-14 px-10 items-center justify-center '>
+    <div className='flex flex-row mt-14 px-10 items-center justify-center bg-darkGray rounded-2xl '>
       <Image
         src={ali1}
         alt='ali'
@@ -13,8 +13,8 @@ export function AboutMe() {
         height={280}
         className='opacity-70 rounded-2xl my-8 !h-[230px]'
       />
-      <div className='flex bg-white rounded-lg px-8 py-4 shadow-md flex-1 mx-8 min-w-[25rem] min-h-[18rem]'>
-        <span className='leading-8 text-navy text-lg text-justify'>
+      <div className='flex  px-8 py-4 shadow-md flex-1 mx-8 min-w-[25rem] min-h-[18rem]'>
+        <span className='leading-8 text-white text-xl leading-10 text-justify'>
           {messages.about}
         </span>
       </div>
--- src/components/PageFeature/Educations.tsx
+++ src/components/PageFeature/Educations.tsx
@@ -1,81 +1,64 @@
-import Image from "next/image";
-import { useAppContext } from "@utils";
-import personal2 from "../../assets/images/2.png";
-import azad from "../../assets/images/azad.png";
-import amol from "../../assets/images/amol.png";
-import beh from "../../assets/images/beh.png";
+import Image from 'next/image';
+import { useAppContext, useEffect } from '@utils';
+import personal2 from '../../assets/images/2.png';
+import azad from '../../assets/images/azad.png';
+import amol from '../../assets/images/amol.png';
+import beh from '../../assets/images/beh.png';
 export function Educations({ isEn }: { isEn: boolean }) {
   const { messages } = useAppContext();
-  const scale = !isEn ? "" : "scale-x-[-1]";
+  const scale = !isEn ? '' : 'scale-x-[-1]';
+
+  const items = [
+    {
+      img: azad,
+      title: messages.masterTitle,
+      date: messages.masterDate,
+      degree: messages.eduTitle,
+    },
+    {
+      img: amol,
+      title: messages.bachelorTitle,
+      date: messages.bachelorDate,
+      degree: messages.eduTitle,
+    },
+    {
+      img: beh,
+      title: messages.associateTitle,
+      date: messages.associateDate,
+      degree: messages.eduTitle,
+    },
+  ];
   return (
     <section>
-      <div className="mt-14 flex flex-col items-center justify-center">
-        <span className="text-pink">{messages.qualification}</span>
-        <span className="text-4xl text-navy font-bold">
+      <div className='mt-14 flex flex-col items-center justify-center'>
+        <span className='text-white'>{messages.qualification}</span>
+        <span className='text-4xl text-yellow font-bold'>
           {messages.educations}
         </span>
       </div>
-      <div className="flex flex-row justify-around mt-8">
+      <div className='flex flex-row justify-around mt-8'>
         <div>
-          <div className="flex flex-row bg-white rounded-lg px-8 py-4 shadow-md min-w-[40rem]">
-            <Image
-              src={azad}
-              alt="tse"
-              width={90}
-              height={50}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.eduTitle}
-              </span>
-              <span className="text-pink">{messages.masterTitle}</span>
-              <span className="text-lightNavy">{messages.masterDate}</span>
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`flex flex-row bg-white rounded-lg px-8 py-4 mt-3 shadow-md edu-item`}
+              style={{
+                animationDelay: `${index * 0.3}s`,
+              }}>
+              <Image
+                src={item.img}
+                alt='school'
+                width={80}
+                height={50}
+                className='object-contain me-4'
+              />
+              <div className='flex flex-col ms-2'>
+                <span className='text-2xl font-bold'>{item.degree}</span>
+                <span className='text-darkGray'>{item.title}</span>
+                <span className='text-yellow'>{item.date}</span>
+              </div>
             </div>
-          </div>
-
-          <div className="flex flex-row bg-white rounded-lg px-8 py-4 mt-3 shadow-md">
-            <Image
-              src={amol}
-              alt="tse"
-              width={80}
-              height={50}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.eduTitle}
-              </span>
-              <span className="text-pink">{messages.bachelorTitle}</span>
-              <span className="text-lightNavy">{messages.bachelorDate}</span>
-            </div>
-          </div>
-
-          <div className="flex flex-row bg-white rounded-lg px-8 py-4 mt-3 shadow-md">
-            <Image
-              src={beh}
-              alt="tse"
-              width={80}
-              height={50}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.eduTitle}
-              </span>
-              <span className="text-pink">{messages.associateTitle}</span>
-              <span className="text-lightNavy">{messages.associateDate}</span>
-            </div>
-          </div>
-        </div>
-        <div className="mt-[-5rem]">
-          <Image
-            src={personal2}
-            alt="ali"
-            width={500}
-            height={400}
-            className={`${scale} mr-[8rem]`}
-          />
+          ))}
         </div>
       </div>
     </section>
