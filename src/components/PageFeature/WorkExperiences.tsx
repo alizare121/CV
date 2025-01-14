090b548176cb5de563d0c9b3b6f6e6c00ccd0819
alizare121 <aliiizaareee@gmail.com>
2025-01-14 17:17:39 +0330

    Fix: re design work expeience section

--- src/components/PageFeature/WorkExperiences.tsx
+++ src/components/PageFeature/WorkExperiences.tsx
@@ -1,37 +1,43 @@
-import Image from "next/image";
-import { useAppContext } from "@utils";
-import tse from "../../assets/images/tse.png";
-import banimood from "../../assets/images/banimode.jpeg";
-import i1st from "../../assets/images/1st.jpeg";
-import iNet from "../../assets/images/inet.png";
-import charisma from "../../assets/images/charisma.jpeg";
+import Image from 'next/image';
+import { useAppContext } from '@utils';
+import tse from '../../assets/images/tse.png';
+import banimood from '../../assets/images/banimode.jpeg';
+import i1st from '../../assets/images/1st.jpeg';
+import iNet from '../../assets/images/inet.png';
+import charisma from '../../assets/images/charisma.jpeg';
 export function WorkExperiences() {
   const { messages } = useAppContext();
 
   return (
-    <div className="flex flex-col w-full mt-4 items-center justify-center">
-      <div className="bg-white flex flex-col w-full rounded-lg p-8 shadow-md">
-        <div className="flex flex-col items-center justify-center w-full">
-          <span className="text-4xl text-navy font-bold">
-            {messages.workExperiences}
-          </span>
-          <span className="text-pink">{messages.workSummeryInfo}</span>
-        </div>
-        <div className="flex flex-col mx-10 mt-10 ">
-          <div className="flex flex-row items-start">
-            <Image
-              src={charisma}
-              alt="charisma"
-              width={90}
-              height={50}
-              className="object-contain animate-pulse"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.charismaJobTitle}
-              </span>
-              <span className="text-pink">{messages.charismaJobDate}</span>
-              <ul className="text-lightNavy list-disc ml-8">
+    <div className='flex flex-col w-full mt-12 items-center justify-center'>
+      <span className='text-4xl text-yellow font-bold'>
+        {messages.workExperiences}
+      </span>
+      <div className='flex flex-col items-center justify-center w-full'>
+        <span className='text-white'>{messages.workSummeryInfo}</span>
+      </div>
+      <div className='flex flex-col w-full mt-6'>
+        <div className='grid grid-cols-12 gap-4'>
+          <div className='flex flex-col items-start bg-white p-8 rounded-2xl col-span-6'>
+            <div className='flex flex row items-center'>
+              <Image
+                src={charisma}
+                alt='charisma'
+                width={90}
+                height={50}
+                className='object-contain'
+              />
+              <div className='flex flex-col gap-2'>
+                <span className='text-2xl font-bold'>
+                  {messages.charismaJobTitle}
+                </span>
+                <span className='bg-yellow px-4 py-1 w-fit rounded-lg '>
+                  {messages.charismaJobDate}
+                </span>
+              </div>
+            </div>
+            <div className='flex flex-col ms-2'>
+              <ul className='list-disc ml-12'>
                 {messages.charismaJobInfo?.map((item: string) => (
                   <li>{item}</li>
                 ))}
@@ -39,20 +45,26 @@ export function WorkExperiences() {
             </div>
           </div>
 
-          <div className="flex flex-row mt-8 items-start">
-            <Image
-              src={tse}
-              alt="tse"
-              width={70}
-              height={40}
-              className="object-contain mx-5"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.tseJobTitle}
-              </span>
-              <span className="text-pink">{messages.tseJobDate}</span>
-              <ul className="text-lightNavy list-disc ml-8">
+          <div className='flex flex-col items-start bg-white p-8 rounded-2xl col-span-6'>
+            <div className='flex flex row items-center'>
+              <Image
+                src={tse}
+                alt='tse'
+                width={70}
+                height={40}
+                className='object-contain mx-5'
+              />
+              <div className='flex flex-col gap-2'>
+                <span className='text-2xl font-bold'>
+                  {messages.tseJobTitle}
+                </span>
+                <span className='bg-yellow px-4 py-1 w-fit rounded-lg '>
+                  {messages.tseJobDate}
+                </span>
+              </div>
+            </div>
+            <div className='flex flex-col ms-2 mt-4'>
+              <ul className='list-disc ml-12'>
                 {messages.tseJobInfo?.map((item: string) => (
                   <li>{item}</li>
                 ))}
@@ -60,20 +72,26 @@ export function WorkExperiences() {
             </div>
           </div>
 
-          <div className="flex flex-row mt-8 items-start">
-            <Image
-              src={banimood}
-              alt="tse"
-              width={90}
-              height={50}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.baniJobTitle}
-              </span>
-              <span className="text-pink">{messages.baniJobDate}</span>
-              <ul className="text-lightNavy list-disc ml-8">
+          <div className='flex flex-col items-start bg-white p-8 rounded-2xl col-span-6'>
+            <div className='flex flex row items-center pl-8'>
+              <Image
+                src={banimood}
+                alt='tse'
+                width={90}
+                height={50}
+                className='object-contain me-4'
+              />
+              <div className='flex flex-col gap-2'>
+                <span className='text-2xl font-bold'>
+                  {messages.baniJobTitle}
+                </span>
+                <span className='bg-yellow px-4 py-1 w-fit rounded-lg '>
+                  {messages.baniJobDate}
+                </span>
+              </div>
+            </div>
+            <div className='flex flex-col ms-2 mt-4'>
+              <ul className='list-disc ml-12'>
                 {messages.baniJobInfo?.map((item: string) => (
                   <li>{item}</li>
                 ))}
@@ -81,20 +99,26 @@ export function WorkExperiences() {
             </div>
           </div>
 
-          <div className="flex flex-row mt-8 items-start">
-            <Image
-              src={i1st}
-              alt="tse"
-              width={90}
-              height={50}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.stJobTitle}
-              </span>
-              <span className="text-pink">{messages.stJobDate}</span>
-              <ul className="text-lightNavy list-disc ml-8">
+          <div className='flex flex-col items-start bg-white p-8 rounded-2xl col-span-6'>
+            <div className='flex flex row items-center pl-8'>
+              <Image
+                src={i1st}
+                alt='tse'
+                width={90}
+                height={50}
+                className='object-contain me-4'
+              />
+              <div className='flex flex-col gap-2'>
+                <span className='text-2xl font-bold'>
+                  {messages.stJobTitle}
+                </span>
+                <span className='bg-yellow px-4 py-1 w-fit rounded-lg '>
+                  {messages.stJobDate}
+                </span>
+              </div>
+            </div>
+            <div className='flex flex-col ms-2 mt-4'>
+              <ul className='list-disc ml-12'>
                 {messages.stJobInfo?.map((item: string) => (
                   <li>{item}</li>
                 ))}
@@ -102,37 +126,53 @@ export function WorkExperiences() {
             </div>
           </div>
 
-          <div className="flex flex-row mt-8 items-start">
-            <Image
-              src={i1st}
-              alt="tse"
-              width={90}
-              height={50}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.stNetJobTitle}
-              </span>
-              <span className="text-pink">{messages.stNetJobDate}</span>
-              <span className="text-lightNavy">{messages.stNetJobInfo}</span>
+          <div className='flex flex-col items-start bg-white p-8 rounded-2xl col-span-6'>
+            <div className='flex flex row items-center pl-8'>
+              <Image
+                src={i1st}
+                alt='tse'
+                width={90}
+                height={50}
+                className='object-contain me-4'
+              />
+              <div className='flex flex-col gap-2'>
+                <span className='text-2xl font-bold'>
+                  {messages.stNetJobTitle}
+                </span>
+                <span className='bg-yellow px-4 py-1 w-fit rounded-lg '>
+                  {messages.stNetJobDate}
+                </span>
+              </div>
+            </div>
+            <div className='flex flex-col ms-2 mt-4'>
+              <ul className='list-disc ml-12'>
+                <li>{messages.stNetJobInfo}</li>
+              </ul>
             </div>
           </div>
 
-          <div className="flex flex-row mt-8 items-start ">
-            <Image
-              src={iNet}
-              alt="tse"
-              width={90}
-              height={70}
-              className="object-contain me-4"
-            />
-            <div className="flex flex-col ms-2">
-              <span className="text-xl text-navy font-bold">
-                {messages.freeJobTitle}
-              </span>
-              <span className="text-pink">{messages.freeJobDate}</span>
-              <span className="text-lightNavy">{messages.freeJobInfo}</span>
+          <div className='flex flex-col items-start bg-white p-8 rounded-2xl col-span-6'>
+            <div className='flex flex row items-center pl-8'>
+              <Image
+                src={iNet}
+                alt='tse'
+                width={90}
+                height={70}
+                className='object-contain me-4'
+              />
+              <div className='flex flex-col gap-2'>
+                <span className='text-2xl font-bold'>
+                  {messages.freeJobTitle}
+                </span>
+                <span className='bg-yellow px-4 py-1 w-fit rounded-lg '>
+                  {messages.freeJobDate}
+                </span>
+              </div>
+            </div>
+            <div className='flex flex-col ms-2 mt-4'>
+              <ul className='list-disc ml-12'>
+                <li>{messages.freeJobInfo}</li>
+              </ul>
             </div>
           </div>
         </div>
