52719b3932c8c7289d04c0e9e947084cc612c27e
Ali Zare <A.zare@charisma.ir>
2025-04-05 14:37:54 +0330

    Feat: Add schema in head

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -7,6 +7,36 @@ export function Header() {
     'رزومه و مجموعه حرفه ای علی زارع را با نمایش مهارت ها، تجربیات و پروژه ها بررسی کنید.';
   const enDescription =
     'Explore the resume and professional portfolio of Ali Zare, showcasing skills, experiences, and projects.';
+
+  const schema = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Ali Zare',
+    jobTitle: 'Frontend Developer',
+    url: 'https://aliiz.ir',
+    image:
+      'https://aliiz.ir/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.1dcd91eb.png&w=828&q=75',
+    sameAs: [
+      'https://www.linkedin.com/in/ali-zare/',
+      'https://github.com/alizare121',
+    ],
+    description:
+      'Skilled frontend developer with over 6 years of experience specializing in JavaScript, TypeScript, and modern frameworks like React and Next.js. Experienced in enhancing code quality and driving technical transformations across multiple projects. Demonstrated ability to lead front-end architecture redesigns and performance optimizations to support business growth. Known for creating intuitive, high-performance UIs and leading cross-functional teams to achieve significant results. Proven record of enhancing UX and optimizing performance, with a deep commitment to continuous learning and delivering high-quality, scalable solutions.',
+    address: {
+      '@type': 'PostalAddress',
+      addressLocality: 'Tehran',
+      addressCountry: 'IR',
+    },
+    email: 'mailto:aliiizaareee@gmail.com',
+    knowsLanguage: ['Persian', 'English', 'German'],
+    knowsAbout: [
+      'JavaScript',
+      'React',
+      'TypeScript',
+      'Frontend Development',
+      'Next js',
+    ],
+  };
   return (
     <Head>
       <title lang='fa'>{faTitle}</title>
@@ -27,6 +57,12 @@ export function Header() {
       <meta property='og:image' content='https://www.aliiz.ir/dev-icon.png' />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:image' content='https://www.aliiz.ir/dev-icon.png' />
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(schema),
+        }}
+      />
     </Head>
   );
 }
